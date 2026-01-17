d857b2009861b76aa894a4b83631b6a2111350bc
kmoeri <kevin.moeri@outlook.com>
2026-01-17 16:25:46 +0100

    adding singleton to load config file values across project files.

---
+++ src/config.py
@@ -0,0 +1,27 @@
+# libraries
+import os
+import toml
+from types import MappingProxyType
+
+# config path
+config_path: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.toml")
+
+
+# recursively load items to make them immutable
+def _load_config_recursively(data: dict):
+    """
+    Helper function to make nested dictionaries immutable (read-only).
+    Example: config['paths'] is read-only, but
+    config['paths']['videos'] would still be mutable. Thus, we recursively load it to protect it.
+    """
+    if isinstance(data, dict):
+        return MappingProxyType({k: _load_config_recursively(v) for k, v in data.items()})
+    return data
+
+
+# load the raw config file
+with open(config_path, "r") as config_file:
+    _raw_config: dict = toml.load(config_file)
+
+# load mapping proxy type config
+config: MappingProxyType = _load_config_recursively(_raw_config)
