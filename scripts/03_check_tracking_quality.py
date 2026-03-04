73dce76bb565739a7d6b6ad33ad826fdab6b07aa
kmoeri <kevin.moeri@outlook.com>
2026-03-04 17:47:17 +0100

    Check the temporal tracking consistency of the hands using CoV.

---
+++ scripts/03_check_tracking_quality.py
@@ -0,0 +1,366 @@
+#!/usr/bin/python3
+# scripts/03_check_tracking_quality.py
+
+# libraries
+import os
+import numpy as np
+import pandas as pd
+
+# modules
+from src.config import config, project_path
+from src.core import Participant, Exercise
+from src.utils import ToolBox
+from src.visualization import Visualizer
+
+
+def determine_best_hand_reference(p: Participant) -> dict:
+    """
+    Calculates the median hand sizes of the left and the right hand sides across all exercises of the given participant
+    for a specific visit (e.g., T1 or T2).
+
+    Args:
+        p (Participant): participant object of the class Participant.
+
+    Returns:
+        best_hand_ref_dict (dict): dictionary holding the largest median hand size for the affected and healthy side
+    """
+
+    # load lists from config file
+    link_lst: list[list[str]] = config['body_parts']['hands_link_lst']
+    l_hand_size_link_lst: list[str] = config['body_parts']['hand_size_left']
+    r_hand_size_link_lst: list[str] = config['body_parts']['hand_size_right']
+
+    def get_med_hand_size(exercise: Exercise, target_side: str, hand_specific_link_lst: list[list[str]],
+                          hand_size_link_lst: list[str], occlusion_threshold: float = 0.85,
+                          framerate: float = 90.0) -> float:
+        """
+        Calculates the median hand size using the anatomical hand size (sum of wrist to middle finger knuckle
+        plus each middle finger segment) and the direct distance (wrist to middle fingertip) as a threshold measure.
+        This function is intended for single hand processing.
+
+        Args:
+            exercise (Exercise): Exercise object with tracked landmarks and exercise information.
+            target_side (str): Whether the active or passive hand is targeted ('L' or 'R').
+            hand_specific_link_lst (list): List of connected landmark pairs, e.g., [['wrist1', 'cmc11'], ...].
+            hand_size_link_lst (list): List of segments from wrist to middle fingertip, e.g., ['wrist1-mcp13', ...].
+            occlusion_threshold: Minimum acceptable ratio of straight-line hand size to segmented hand size.
+                                 (e.g., 0.7 means straight line must be at least 70% of the segmented length).
+            framerate (float): Number of frames per second of the underlying data.
+
+        Returns:
+            median_hand_size (float): The median size of the given hand.
+        """
+
+        # length of each hand segment
+        tb: ToolBox = ToolBox()
+        exercise_dict: dict = exercise.clean_hand_landmarks
+
+        df_cols = {}
+        for label, axes in exercise_dict.items():
+            if label == 'frame':                    # do not extract the column named 'frame' (usually first)
+                continue
+            df_cols[f'{label}_x'] = axes[0]
+            df_cols[f'{label}_y'] = axes[1]
+            df_cols[f'{label}_z'] = axes[2]
+
+        exercise_df: pd.DataFrame = pd.DataFrame(df_cols)
+        segment_len_df: pd.DataFrame = tb.calculate_3d_segment_lengths(exercise_df, hand_specific_link_lst)
+
+        # calculate the hand size
+        try:
+            # get handedness: index 1 -> left hand, index 2 -> right hand. First column is 'wrist1' or 'wrist2'
+            side: int = 1 if target_side == 'L' else 2
+
+            # drop rows where any of the segments to calculate the hand size are missing (NaN)
+            clean_segment_len: pd.DataFrame = segment_len_df.dropna(subset=hand_size_link_lst)
+
+            # distance from wrist to fingertip by the sum of each segment inbetween
+            hand_size_segment_len: pd.Series = clean_segment_len[hand_size_link_lst].sum(axis=1)
+
+            # distance from wrist to fingertip by direct connection
+            wrist_middle_finger_dist = tb.calculate_3d_segment_lengths(exercise_df.loc[clean_segment_len.index],
+                                                                       [[f'wrist{side}', f'ftip{side}3']])
+        except KeyError as e:
+            print(
+                f"Error: Could not calculate hand size. Missing one or more required middle finger segment columns: {e}")
+            del tb
+            return 0.0
+
+        # direct wrist to middle fingertip distance
+        hand_size_direct_len: pd.Series = wrist_middle_finger_dist[f'wrist{side}-ftip{side}3']
+
+        # occlusion filter: hand size segment lengths of flexed hands, i.e., fists should not be included
+        diff_dist_series: pd.Series = hand_size_segment_len - hand_size_direct_len
+        if (diff_dist_series.values < 0).any():
+            print('Warning: tracking detected abnormal values for hand size calculation. '
+                  'Direct wrist-ftip distance was larger than sum of segments.')
+
+        # occlusion metric
+        occlusion_ratio = hand_size_direct_len / hand_size_segment_len
+        filt_mask_lst = occlusion_ratio > occlusion_threshold
+
+        # find at least a sum of frames equal or larger than 1s of 'flat' hand positions (e.g., 90 fps -> 90 frames)
+        if sum(filt_mask_lst) < int(framerate):
+            print(f'Warning: current exercise was skipped for median hand size calculation.'
+                  f'Not enough flat hand positions found (found: {sum(filt_mask_lst)}).')
+            del tb
+            return 0.0
+
+        # get the median hand size of the 'flat' hand positions
+        median_hand_size: float = np.median(hand_size_segment_len[filt_mask_lst].tolist())
+
+        del tb
+        return median_hand_size
+
+    # variables for results
+    best_hand_ref_dict: dict = dict()
+    med_results = {'L': [], 'R': []}
+
+    # run for each exercise
+    for ex_key in p.exercises.keys():
+
+        # extract side of focus
+        hand_of_focus: str = p.exercises[ex_key].side_focus
+
+        # determine the links based on the side of focus ('L' or 'R')
+        if hand_of_focus == 'L':
+            passive_hand = 'R'
+            active_hand_link_lst: list = link_lst[:len(link_lst)//2]
+            passive_hand_link_lst: list = link_lst[len(link_lst)//2:]
+            active_hand_size_link_lst: list = l_hand_size_link_lst
+            passive_hand_size_link_lst: list = r_hand_size_link_lst
+
+        else:
+            passive_hand = 'L'
+            active_hand_link_lst: list = link_lst[len(link_lst)//2:]
+            passive_hand_link_lst: list = link_lst[:len(link_lst)//2]
+            active_hand_size_link_lst: list = r_hand_size_link_lst
+            passive_hand_size_link_lst: list = l_hand_size_link_lst
+
+        # relax the threshold for more severe spasticity affecting the hand
+        thresh_lst = config['tracking_consistency']['thresh_lst']
+        active_med_hand_size: float = 0.0
+        passive_med_hand_size: float = 0.0
+
+        # calculate median hand sizes for different hand aperture threshold until return variable is > 0.0
+        # active side
+        for thresh in thresh_lst:
+            active_med_hand_size = get_med_hand_size(p.exercises[ex_key],
+                                                     target_side=hand_of_focus,
+                                                     hand_specific_link_lst=active_hand_link_lst,
+                                                     hand_size_link_lst=active_hand_size_link_lst,
+                                                     occlusion_threshold=thresh,
+                                                     framerate=config['camera_param']['fps'])
+
+            if active_med_hand_size > 0.0:
+                break
+
+        # passive side
+        for thresh in thresh_lst:
+            passive_med_hand_size = get_med_hand_size(p.exercises[ex_key],
+                                                      target_side=passive_hand,
+                                                      hand_specific_link_lst=passive_hand_link_lst,
+                                                      hand_size_link_lst=passive_hand_size_link_lst,
+                                                      occlusion_threshold=thresh,
+                                                      framerate=config['camera_param']['fps'])
+
+            if passive_med_hand_size > 0.0:
+                break
+
+            if active_med_hand_size == 0.0 or passive_med_hand_size == 0.0:
+                print(f'Warning: Exercise "{ex_key}" of participant {p.pid} for visit {p.visit_id} yielded no median for either hand size.')
+
+        med_results[hand_of_focus].append(active_med_hand_size)
+        med_results[passive_hand].append(passive_med_hand_size)
+
+    # select the largest median hand size
+    max_left_hand_size: float = max(med_results['L']) if med_results['L'] else 0.0
+    max_right_hand_size: float = max(med_results['R']) if med_results['R'] else 0.0
+
+    # add the selected hand size to the dictionary
+    if p.affected_side == 'L':
+        best_hand_ref_dict[p.pid] = {'Affected': max_left_hand_size, 'Healthy': max_right_hand_size}
+
+    elif p.affected_side == 'R':
+        best_hand_ref_dict[p.pid] = {'Affected': max_right_hand_size, 'Healthy': max_left_hand_size}
+
+    return best_hand_ref_dict
+
+
+def get_hand_segment_stability(p: Participant, ref_hand_size_dict: dict) -> pd.DataFrame:
+    """
+    Calculates segment lengths and normalizes them by anatomical hand size (sum of wrist to middle finger knuckle plus
+    each middle finger segment). Calculates the Coefficient of Variance (CoV) for each body segment for both the
+    active and the passive hands.
+
+    Args:
+        p (Participant): list of participant objects of the class Participant.
+        ref_hand_size_dict (dict): dictionary of reference hand sizes for each participant.
+
+    Returns:
+        pd.dataframe: A dataframe containing the CoV of each body segment for both hands.
+    """
+
+    # configuration definitions
+    finger_names_dict = {'1': 'Thumb', '2': 'Index', '3': 'Middle', '4': 'Ring', '5': 'Pinky'}
+    link_lst: list[list[str]] = config['body_parts']['hands_link_lst']
+
+    # tracking consistency test using all participants and exercises
+    results_lst: list = []
+    tb: ToolBox = ToolBox()
+
+    # get the reference dict of the current participant
+    participant_ref_dict: dict = ref_hand_size_dict.get(p.pid, {})
+
+    # run for each exercise
+    for ex_key in p.exercises.keys():
+
+        # determine the active and passive side ('L' or 'R')
+        active_side: str = p.exercises[ex_key].side_focus
+        passive_side: str = 'R' if active_side == 'L' else 'L'
+
+        # map the active and passive sides to the affected and healthy sides by participant info
+        active_condition: str = 'Affected' if active_side == p.affected_side else 'Healthy'
+        passive_condition: str = 'Healthy' if active_side == p.affected_side else 'Affected'
+
+        # configuration for the roles of both hands for a given exercise
+        hand_roles = {
+            'Active': {
+                'side': active_side,
+                'condition': active_condition,
+                'link_idx': link_lst[:len(link_lst)//2] if active_side == 'L' else link_lst[len(link_lst)//2:]
+            },
+            'Passive': {
+                'side_lr': passive_side,
+                'condition': passive_condition,
+                'link_idx': link_lst[:len(link_lst)//2] if passive_side == 'L' else link_lst[len(link_lst)//2:]
+            }
+        }
+
+        # load the exercise values in a dataframe
+        exercise_dict = p.exercises[ex_key].clean_hand_landmarks
+        df_cols = {}
+
+        for label, axes in exercise_dict.items():
+            if label == 'frame':
+                continue
+            df_cols[f'{label}_x'] = axes[0]
+            df_cols[f'{label}_y'] = axes[1]
+            df_cols[f'{label}_z'] = axes[2]
+
+        exercise_df: pd.DataFrame = pd.DataFrame(df_cols)
+
+        for role, role_info in hand_roles.items():
+            curr_link_lst = role_info['link_idx']
+            condition = role_info['condition']
+
+            # get the median hand size using the correct keys ('Affected' or 'Healthy')
+            med_hand_size: float = participant_ref_dict.get(condition, 0.0)
+
+            # handle missing median hand size
+
+            if med_hand_size == 0.0:
+                opposite_condition = 'Healthy' if condition == 'Affected' else 'Affected'
+                med_hand_size = participant_ref_dict.get(opposite_condition, 0.0)
+
+                if med_hand_size > 0.0:
+                    print(f'Warning: Participant {p.pid} used the "{opposite_condition}" hand size to normalize '
+                          f'the "{condition}" ({role}) segments in "{ex_key}".')
+                else:
+                    print(f'Error: No median hand size for participant {p.pid}. Skipping {role} hand in {ex_key}.')
+                    continue
+
+            # calculate the length of each hand segment
+            segment_len_df: pd.DataFrame = tb.calculate_3d_segment_lengths(exercise_df, curr_link_lst)
+
+            # anatomical normalization - only needed when comparing hand sizes between participants, otherwise no effect.
+            norm_segment_df: pd.DataFrame = segment_len_df.div(med_hand_size, axis=0)
+
+            # calculation of segment CoV (%)
+            body_segments_cov_series: pd.Series = ((norm_segment_df.std() / norm_segment_df.mean()) * 100)
+
+            for segment_name, cov_value in body_segments_cov_series.items():
+                finger_name = finger_names_dict.get(str(segment_name)[-1], 'Unknown')
+
+                results_lst.append({
+                    'Participant': p.pid,                   # e.g, P001
+                    'Trial': ex_key,                        # e.g., 'FingerTapping_Affected'
+                    'Active_Side': active_side,             # 'L' or 'R'
+                    'Passive_Side': passive_side,           # 'L' or 'R'
+                    'Hand_Role': role,                      # 'Active' or 'Passive'
+                    'Hand_Condition': condition,            # 'Affected' or 'Healthy'
+                    'Finger': finger_name,                  # 'Thumb', 'Index', 'Middle', 'Ring', 'Pinky'
+                    'Segment': segment_name,                # e.g., 'wrist1-mcp13'
+                    'CoV': round(cov_value, 3)
+                })
+
+    return pd.DataFrame(results_lst)
+
+
+def run_temporal_consistency_check():
+
+    # load participant objects
+    participant_objs_path: str = os.path.join(project_path, 'data', '03_processed')
+    participant_pickle_lst: list = [x for x in sorted(os.listdir(participant_objs_path))
+                                    if x.startswith('P') and x.endswith('.pickle')]
+
+    all_cov_df_lst: list = []
+
+    # run for each participant and each visit
+    for pickle_file in participant_pickle_lst:
+        p: Participant = Participant.load(os.path.join(participant_objs_path, pickle_file))
+
+        # determine coefficients of variation for each hand segment
+        hand_size_dict: dict = determine_best_hand_reference(p)
+        cov_res_df: pd.DataFrame = get_hand_segment_stability(p, hand_size_dict)
+        all_cov_df_lst.append(cov_res_df)
+
+    # combine all participants into one dataframe
+    full_cov_df: pd.DataFrame = pd.concat(all_cov_df_lst, ignore_index=True)
+
+    # order of fingers for plotting
+    finger_order = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+
+    # get the average coefficients of variance of each finger grouping by condition and role
+    finger_df: pd.DataFrame = full_cov_df.groupby(['Participant', 'Trial', 'Hand_Condition', 'Hand_Role', 'Finger']
+                                                  )['CoV'].mean().reset_index()
+
+    # create combined 'State' column for the 4-way comparison
+    finger_df['State'] = finger_df['Hand_Condition'] + " (" + finger_df['Hand_Role'] + ")"
+
+    # pivot DataFrame table to adjust parameters for raincloud plot
+    reformat_finger_df: pd.DataFrame = finger_df.pivot_table(
+        index=['Participant', 'Trial', 'Hand_Condition', 'Hand_Role'],
+        columns='Finger',
+        values='CoV'
+    ).reset_index()
+
+    # filter states for Raincloud plots
+    active_healthy_df = reformat_finger_df[(reformat_finger_df['Hand_Condition'] == 'Healthy') &
+                                           (reformat_finger_df['Hand_Role'] == 'Active')]
+    active_affected_df = reformat_finger_df[(reformat_finger_df['Hand_Condition'] == 'Affected') &
+                                            (reformat_finger_df['Hand_Role'] == 'Active')]
+
+    # create visualizer object
+    vis_util: Visualizer = Visualizer()
+
+    # plot raincloud (Active Healthy)
+    vis_util.vis_consistency_raincloud(link_df=active_healthy_df,
+                                       columns_to_plot=finger_order,
+                                       title='Task Consistency of Healthy Hand (Active)',
+                                       x_label='Finger',
+                                       y_label='Mean CoV (%)')
+
+    # plot raincloud (affected)
+    vis_util.vis_consistency_raincloud(link_df=active_affected_df,
+                                       columns_to_plot=finger_order,
+                                       title='Task Consistency of Affected Hand (Active)',
+                                       x_label='Finger',
+                                       y_label='Mean CoV (%)')
+
+    # box plot comparison of all 4 states
+    vis_util.viz_comparison_boxplot(finger_df, finger_order)
+
+
+if __name__ == '__main__':
+    run_temporal_consistency_check()
---
+++ src/visualization.py
@@ -0,0 +1,259 @@
+# src/visualization.py
+
+# libraries
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+# modules
+from src.config import config, project_path
+
+
+class Visualizer:
+    def __init__(self) -> None:
+        # main results directory (parent)
+        self.results_path = os.path.join(project_path, 'data', '05_results')
+        os.makedirs(self.results_path, exist_ok=True)
+        # temporal consistency results directory (child)
+        self.temp_consistency_res_path = os.path.join(self.results_path, '02_temporal_consistency')
+        os.makedirs(self.temp_consistency_res_path, exist_ok=True)
+        # feature extraction results directory (child)
+        self.features_res_path = os.path.join(self.results_path, '03_feature_extraction')
+        os.makedirs(self.features_res_path, exist_ok=True)
+        # classification results directory (child)
+        self.classification_res_path = os.path.join(self.results_path, '04_classification')
+        os.makedirs(self.classification_res_path, exist_ok=True)
+
+    # ============================================================================= #
+    #                            2) TEMPORAL CONSISTENCY                            #
+    # ============================================================================= #
+    def vis_consistency_raincloud(self, link_df: pd.DataFrame, columns_to_plot: list[str],
+                                   title: str = '', x_label: str = '', y_label: str = '') -> None:
+
+        sns.set_theme(style="whitegrid")
+        link_palette = sns.color_palette("Set1", 5)
+
+        if not columns_to_plot:
+            print("Error: columns_to_plot list is empty.")
+            return
+
+        # prepare Data
+        plot_columns = columns_to_plot[:min(len(columns_to_plot), 5)]
+
+        # map columns to colors
+        column_colors = dict(zip(plot_columns, link_palette))
+
+        fig, ax = plt.subplots(figsize=(1.8 * len(plot_columns), 6))
+
+        # loop through each column to plot one by one
+        for i, link_name in enumerate(plot_columns):
+
+            # extract data for the current column
+            subset = link_df[link_name].dropna()
+            color = column_colors[link_name]
+
+            # violin plot representing the density
+            violin_plot = sns.violinplot(
+                x=[i] * len(subset), y=subset,
+                inner=None, cut=0, bw_method=0.2, linewidth=0,
+                color=color, ax=ax, alpha=0.6,
+                zorder=1
+            )
+
+            # mask left half of violin plot
+            try:
+                violin = ax.collections[-1]
+                path = violin.get_paths()[0]
+                vertices = path.vertices
+                # find the mean X position of the vertices for the center line
+                mean_x = np.mean(vertices[:, 0])
+                # set all X-vertices to be AT LEAST the mean X, effectively clipping the left side
+                vertices[:, 0] = np.maximum(vertices[:, 0], mean_x)
+
+            except IndexError:
+                # handle case where violin plot may fail if data is too sparse
+                print(f"Warning: Could not mask violin for {link_name}.")
+                pass
+
+            # boxplot
+            sns.boxplot(
+                x=[i] * len(subset),
+                y=subset,
+                whis=1.5,
+                width=0.1,
+                showcaps=True,
+                boxprops={'facecolor': 'white', 'edgecolor': 'black', 'zorder': 3},
+                whiskerprops={'color': 'black', 'zorder': 3},
+                flierprops={'marker': ''},
+                medianprops={'color': 'black', 'zorder': 3},
+                ax=ax
+            )
+
+            # stripplot
+            # jitter = np.random.normal(loc=-0.2, scale=0.05, size=len(subset))
+            jitter = np.random.uniform(low=-0.3, high=-0.1, size=len(subset))
+            ax.scatter(
+                np.full_like(subset, i) + jitter,
+                subset,
+                color=color, alpha=0.3, s=30, edgecolor='none',
+                zorder=2
+            )
+
+        ax.set_xticks(range(len(plot_columns)))
+        ax.set_xticklabels([name.replace('-', ' ').title() for name in plot_columns])
+        ax.set_xlabel(x_label, fontsize=12)
+        ax.set_ylabel(y_label, fontsize=12)
+
+        # y-axis limits and steps
+        y_min = 0.0
+        y_max = 100.0
+        y_step = 10.0
+
+        ax.set_ylim(y_min, y_max)
+        ax.set_yticks(np.arange(y_min, y_max + y_step, y_step))
+
+        fig.suptitle(title, fontsize=14, y=0.98)
+        plt.tight_layout()
+
+        suffix = '.png'
+        f_name: str = title.replace(' ', '_') + suffix
+        f_path: str = os.path.join(self.temp_consistency_res_path, f_name)
+        if not os.path.exists(f_path):
+            plt.savefig(f_path, format=suffix[1:], dpi=600)
+
+        plt.close()
+
+    def viz_comparison_subplots(self, finger_df, finger_order: list[str]) -> None:
+
+        fig, axes = plt.subplots(1, 2, figsize=(16, 6), gridspec_kw={'width_ratios': [1, 1]})
+
+        # subplot 1: grouped boxplot
+        sns.boxplot(data=finger_df,
+                    x='Finger',
+                    y='CoV',
+                    hue='Side',
+                    ax=axes[0],
+                    palette='Set2',
+                    order=finger_order)
+
+        axes[0].set_title('Comparison: Healthy vs. Affected Side')
+        axes[0].set_ylabel('Mean CoV (%)')
+
+        # subplot 2: raincloud of affected side - violin/strip combination
+        df_aff = finger_df[finger_df['Side'] == 'Affected']
+        sns.violinplot(
+            data=df_aff, x='Finger', y='CoV',
+            ax=axes[1], inner=None, color=".8", order=['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+        )
+        sns.stripplot(
+            data=df_aff, x='Finger', y='CoV',
+            ax=axes[1], alpha=0.4, order=['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+        )
+        axes[1].set_title('Distribution of Affected Side Stability')
+        axes[1].set_ylabel('Mean CoV (%)')
+
+        plt.tight_layout()
+
+        suffix = '.png'
+        f_name: str = 'Distribution_of_Affected_Side_Stability' + suffix
+        f_path: str = os.path.join(self.temp_consistency_res_path, f_name)
+        if not os.path.exists(f_path):
+            plt.savefig(f_path, format=suffix[1:], dpi=600)
+
+        plt.close()
+
+    def viz_comparison_boxplot(self, finger_df: pd.DataFrame, finger_order: list[str]) -> None:
+
+        plt.figure(figsize=(12, 6))
+
+        # custom palette for the four states
+        custom_palette = {
+            'Healthy (Active)': '#1f77b4',  # dark blue
+            'Healthy (Passive)': '#aec7e8', # light blue
+            'Affected (Active)': '#d62728', # dark red
+            'Affected (Passive)': '#ff9896' # light red
+        }
+
+        # order of the legend and bars
+        hue_order = ['Healthy (Active)', 'Healthy (Passive)', 'Affected (Active)', 'Affected (Passive)']
+
+        sns.boxplot(data=finger_df,
+                    x='Finger',
+                    y='CoV',
+                    hue='State',
+                    palette=custom_palette,
+                    hue_order=hue_order,
+                    order=finger_order)
+
+        plt.title('Temporal Consistency: Active vs. Passive Hand', fontsize=14)
+        plt.ylabel('Mean CoV (%)', fontsize=12)
+        plt.xlabel('Finger', fontsize=12)
+
+        # legend placed outside the plot to prevent it from covering data
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', title='Hand State')
+
+        plt.yticks(np.arange(0, 100 + 10, 10))
+
+        plt.tight_layout()
+
+        suffix = '.png'
+        f_name: str = '4-Way_Consistency_Boxplot' + suffix
+        f_path: str = os.path.join(self.temp_consistency_res_path, f_name)
+        if not os.path.exists(f_path):
+            plt.savefig(f_path, format=suffix[1:], dpi=600)
+
+        plt.close()
+
+    def viz_normality_qq_plots(self, paired_df: pd.DataFrame, finger_order: list[str]) -> None:
+
+        # create a 3x2 grid
+        fig, axes = plt.subplots(3, 2, figsize=(10, 15))
+
+        # flatten the 2D array so we can use a single index 'i'
+        axes_flat = axes.flatten()
+
+        for i, finger in enumerate(finger_order):
+            # select the specific subplot for this finger
+            ax = axes_flat[i]
+
+            finger_data = paired_df[paired_df['Finger'] == finger]
+            differences = (finger_data['Healthy'] - finger_data['Affected']).dropna()
+            print(f'N: {len(differences)}')
+            if len(differences) >= 3:
+                _, p_val = stats.shapiro(differences)
+                norm_text = f"Shapiro p={p_val:.3f}"
+                stats.probplot(differences, dist="norm", plot=ax)
+            else:
+                norm_text = "N too small"
+                ax.text(0.5, 0.5, 'Insufficient Data', ha='center')
+
+            ax.set_title(f'Q-Q Plot: {finger}\n{norm_text}')
+
+            # formatting for a 2-column layout
+            ax.set_ylabel('Sample Quantiles' if i % 2 == 0 else '')
+            ax.set_xlabel('Theoretical Quantiles')
+
+        # hide the 6th empty subplot
+        if len(finger_order) < len(axes_flat):
+            axes_flat[-1].axis('off')
+
+        plt.tight_layout()
+
+        suffix = '.png'
+        f_name: str = 'QQ_Plots' + suffix
+        f_path: str = os.path.join(self.temp_consistency_res_path, f_name)
+        if not os.path.exists(f_path):
+            plt.savefig(f_path, format=suffix[1:], dpi=600)
+
+        plt.close()
+
+    # ============================================================================= #
+    #                            3) PARAMETER EXTRACTION                            #
+    # ============================================================================= #
+
+    # ============================================================================= #
+    #                            4) HELPER FUNCTION                                 #
+    # ============================================================================= #
