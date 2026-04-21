Fixed: Hand size calculation moved to exercise level in core.py and is calculated by the sum of landmark links between the respective wrist and middle fingertip

scripts/04_extract_kinematics.py
@@ -3,11 +3,11 @@
 
 # libraries
 import os
-import numpy as np
+from tqdm import tqdm
 
 # modules
 from src.config import config, project_path
-from src.core import Participant, Exercise
+from src.core import Participant
 from src.exercise_evaluation import ExerciseEvaluator
 from src.utils import save_extracted_data_to_csv
 
@@ -25,34 +25,8 @@ def run_kinematics_extractor(save_plots: bool = True):
     all_extracted_features_lst: list = []
 
     # run for each participant and each visit
-    for pickle_file in participant_pickle_lst:
+    for pickle_file in tqdm(participant_pickle_lst, desc='Extracting Kinematic Features'):
         p: Participant = Participant.load(os.path.join(participant_objs_path, pickle_file))
-
-        # calculate the global hand size (median)
-        left_hand_sizes: list = []
-        right_hand_sizes: list = []
-
-        # iterate across each exercise for a given participant
-        for ex in p.exercises.values():
-            lms = ex.clean_hand_landmarks
-            if not lms:
-                continue
-
-            # calculate the median hand size for the left hand (distance: wrist1 - mcp13)
-            if 'wrist1' in lms and 'mcp13' in lms:
-                diff_left: np.ndarray = np.array(lms['wrist1']) - np.array(lms['mcp13'])
-                dist_left: np.ndarray = np.linalg.norm(diff_left, axis=0)
-                left_hand_sizes.extend(dist_left)
-
-            # calculate the median hand size for the right hand (distance: wrist1 - mcp13)
-            if 'wrist2' in lms and 'mcp23' in lms:
-                diff_right: np.ndarray = np.array(lms['wrist2']) - np.array(lms['mcp23'])
-                dist_right: np.ndarray = np.linalg.norm(diff_right, axis=0)
-                right_hand_sizes.extend(dist_right)
-
-        # add the median value to the hand size attribute of the Participant object
-        p.left_hand_size = float(np.median(left_hand_sizes)) if left_hand_sizes else 0.0
-        p.right_hand_size = float(np.median(right_hand_sizes)) if right_hand_sizes else 0.0
 
         # loop through every exercise this participant performed
         for ex_key, exercise in p.exercises.items():
@@ -66,39 +40,41 @@ def run_kinematics_extractor(save_plots: bool = True):
                              'side_condition': exercise.side_condition,
                              'cam_ID': exercise.cam_id}
 
-            # Determine which hand size to pass to the evaluator
-            active_hand_size = p.left_hand_size if exercise.side_focus == 'L' else p.right_hand_size
+            # get the hand size
+            active_hand_size: float = exercise.left_hand_size if exercise.side_focus == 'L' else exercise.right_hand_size
+
+            # safety fallback
+            if active_hand_size == 0.0:
+                print(f'Warning: Hand size 0.0 for {ex_key}. Check loader output.')
+                active_hand_size = 1e-8
 
             try:
                 metrics: dict = {}
                 if 'FingerTapping' in ex_key:
                     metrics = ex_eval.analyze_finger_tapping(exercise, p.pid, active_hand_size, save_plots)
-                    exercise.metrics = metrics
                 elif 'FingerAlternation' in ex_key:
                     metrics = ex_eval.analyze_finger_alternation(exercise, active_hand_size, save_plots)
-                    exercise.metrics = metrics
                 elif 'HandOpening' in ex_key:
                     metrics = ex_eval.analyze_hand_opening(exercise, p.pid, active_hand_size, save_plots)
-                    exercise.metrics = metrics
                 elif 'ProSup' in ex_key:
                     metrics = ex_eval.analyze_pronation_supination(exercise, p.pid, save_plots)
-                    exercise.metrics = metrics
 
                 # add the extracted metrics to the metadata
-                row_meta_data.update(metrics)
-
-                # append the row data to the main list
-                all_extracted_features_lst.append(row_meta_data)
+                if metrics:
+                    exercise.metrics = metrics
+                    row_meta_data.update(metrics)
+                    all_extracted_features_lst.append(row_meta_data)
 
             except Exception as e:
                 print(f'Error extracting {ex_key} for {p.pid}: {e}')
 
         # 'update' (overwrites) the participant object file with the new metrics data.
         p.save(participant_objs_path)
-        print(f"Successfully updated pickle file for {p.pid}_{p.visit_id}")
 
     # save the extracted features to a csv file
-    save_extracted_data_to_csv(all_extracted_features_lst, out_dir=os.path.join(project_path, 'data', '04_features'))
+    feature_dir: str = os.path.join(project_path, 'data', '04_features')
+    os.makedirs(feature_dir, exist_ok=True)
+    save_extracted_data_to_csv(all_extracted_features_lst, out_dir=feature_dir)
 
 
 if __name__ == '__main__':

scripts/05_check_kinematics.py
@@ -3,6 +3,10 @@
 
 # libraries
 import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from scipy.spatial.transform import Rotation
 
 # modules
 from src.config import project_path
@@ -10,101 +14,142 @@ from src.core import Participant, Exercise
 from src.visualization import Visualizer
 
 
-def _prepare_dashboard_data(exercise: Exercise) -> dict:
-    """
-    Extracts the 3D landmarks of hands and pose including the 1D metrics for the dashboard.
-    Returns a dictionary comprising the items necessary for rendering the dashboard:
-    - Pose Landmarks
-    - Active Hand Landmarks
-    - Passive Hand Landmarks
-    - Time series of the metrics extracted from the respective exercise
-    """
+# def _prepare_dashboard_data(exercise: Exercise) -> dict:
+#     """
+#     Extracts the 3D landmarks of hands and pose including the 1D metrics for the dashboard.
+#     Returns a dictionary comprising the items necessary for rendering the dashboard:
+#     - Pose Landmarks
+#     - Active Hand Landmarks
+#     - Passive Hand Landmarks
+#     - Time series of the metrics extracted from the respective exercise
+#     """
+#
+#     # filter the Pose landmarks (keep only upper limbs)
+#     desired_pose_lms = [
+#         'shoulder_left', 'shoulder_right',
+#         'elbow_left', 'elbow_right',
+#         'wrist_left', 'wrist_right'
+#     ]
+#     filtered_pose = {k: v for k, v in exercise.clean_pose_landmarks.items() if k in desired_pose_lms}
+#
+#     # helper function to find the first digit in the landmark name (e.g., 'ftip15' -> returns '1')
+#     def get_hand_id(key_str):
+#         for char in key_str:
+#             if char.isdigit():
+#                 return char
+#         return None
+#
+#     # split the hands landmark dictionary using the left-right coding convention (1: Left, 2: Right)
+#     left_hand_lm: dict = {k: v for k, v in
+#                           getattr(exercise, 'aligned_hand_landmarks', exercise.clean_hand_landmarks).items() if
+#                           get_hand_id(k) == '1'}
+#     right_hand_lm: dict = {k: v for k, v in
+#                            getattr(exercise, 'aligned_hand_landmarks', exercise.clean_hand_landmarks).items() if
+#                            get_hand_id(k) == '2'}
+#
+#     # create dashboard data dict template
+#     dashboard_data: dict = {'pose': filtered_pose,
+#                             'left_hand': left_hand_lm,
+#                             'right_hand': right_hand_lm,
+#                             'metrics': {}                   # {'ExerciseName': (time_array, signal_array)}
+#                             }
+#
+#     # associate hand side to active/passive based on the exercise focus
+#     if exercise.side_focus == 'L':
+#         dashboard_data['active_hand'] = left_hand_lm
+#         dashboard_data['passive_hand'] = right_hand_lm
+#     else:
+#         dashboard_data['active_hand'] = right_hand_lm
+#         dashboard_data['passive_hand'] = left_hand_lm
+#
+#     # extract the metrics
+#     metrics = getattr(exercise, 'metrics', {})
+#     if not metrics:
+#         return dashboard_data
+#
+#     ex_id = exercise.exercise_id
+#
+#     # add the exercise metrics based on the exercise name
+#     if 'FingerTapping' in ex_id:
+#         t = metrics.get('idx_tap_time_series_x')
+#         y = metrics.get('idx_tap_time_series_y')
+#         if t is not None and y is not None:
+#             dashboard_data['metrics']['Index-Thenar_Distance'] = (t, y)
+#
+#     elif 'HandOpening' in ex_id:
+#         t = metrics.get('open_close_time_series_x')
+#         y = metrics.get('open_close_time_series_y')
+#         if t is not None and y is not None:
+#             dashboard_data['metrics']['Mean-Finger-Wrist_Distance'] = (t, y)
+#
+#     elif 'ProSup' in ex_id:
+#         t = metrics.get('pro_sup_time_series_x')
+#         y = metrics.get('pro_sup_time_series_y')
+#         if t is not None and y is not None:
+#             dashboard_data['metrics']['Pronation-Supination_Rotation-Angle'] = (t, y)
+#
+#     # FingerAlternation
+#
+#     return dashboard_data
 
-    # filter the Pose landmarks (keep only upper limbs)
-    desired_pose_lms = [
-        'shoulder_left', 'shoulder_right',
-        'elbow_left', 'elbow_right',
-        'wrist_left', 'wrist_right'
-    ]
-    filtered_pose = {k: v for k, v in exercise.clean_pose_landmarks.items() if k in desired_pose_lms}
-
-    # helper function to find the first digit in the landmark name (e.g., 'ftip15' -> returns '1')
-    def get_hand_id(key_str):
-        for char in key_str:
-            if char.isdigit():
-                return char
-        return None
-
-    # split the hands landmark dictionary using the left-right coding convention (1: Left, 2: Right)
-    left_hand_lm: dict = {k: v for k, v in
-                          getattr(exercise, 'aligned_hand_landmarks', exercise.clean_hand_landmarks).items() if
-                          get_hand_id(k) == '1'}
-    right_hand_lm: dict = {k: v for k, v in
-                           getattr(exercise, 'aligned_hand_landmarks', exercise.clean_hand_landmarks).items() if
-                           get_hand_id(k) == '2'}
-
-    # create dashboard data dict template
-    dashboard_data: dict = {'pose': filtered_pose,
-                            'left_hand': left_hand_lm,
-                            'right_hand': right_hand_lm,
-                            'metrics': {}                   # {'ExerciseName': (time_array, signal_array)}
-                            }
-
-    # associate hand side to active/passive based on the exercise focus
-    if exercise.side_focus == 'L':
-        dashboard_data['active_hand'] = left_hand_lm
-        dashboard_data['passive_hand'] = right_hand_lm
-    else:
-        dashboard_data['active_hand'] = right_hand_lm
-        dashboard_data['passive_hand'] = left_hand_lm
+def _extract_plot_metrics(exercise: Exercise) -> dict:
 
     # extract the metrics
     metrics = getattr(exercise, 'metrics', {})
     if not metrics:
-        return dashboard_data
+        return {}
 
     ex_id = exercise.exercise_id
+    plot_metrics = {}
 
     # add the exercise metrics based on the exercise name
     if 'FingerTapping' in ex_id:
         t = metrics.get('idx_tap_time_series_x')
         y = metrics.get('idx_tap_time_series_y')
-        if t is not None and y is not None:
-            dashboard_data['metrics']['Index-Thenar_Distance'] = (t, y)
+        if t is not None and y is not None and len(t) > 0:
+            plot_metrics['Index-Thenar_Distance'] = (t, y)
+
+    elif 'FingerAlternation' in ex_id:
+        t = metrics.get('alt_tap_time_series_x')
+        y = metrics.get('alt_tap_time_series_y')
+        # returns a list of arrays (one for each finger)
+        if t is not None and y is not None and len(t) > 0:
+            plot_metrics['Alternating_Finger_Distances'] = (t, y)
 
     elif 'HandOpening' in ex_id:
         t = metrics.get('open_close_time_series_x')
         y = metrics.get('open_close_time_series_y')
-        if t is not None and y is not None:
-            dashboard_data['metrics']['Mean-Finger-Wrist_Distance'] = (t, y)
+        if t is not None and y is not None and len(t) > 0:
+            plot_metrics['Mean-Finger-Wrist_Distance'] = (t, y)
 
     elif 'ProSup' in ex_id:
         t = metrics.get('pro_sup_time_series_x')
         y = metrics.get('pro_sup_time_series_y')
-        if t is not None and y is not None:
-            dashboard_data['metrics']['Pronation-Supination_Rotation-Angle'] = (t, y)
+        if t is not None and y is not None and len(t) > 0:
+            plot_metrics['Pronation-Supination_Rotation-Angle'] = (t, y)
 
-    # FingerAlternation
-
-    return dashboard_data
+    return plot_metrics
 
 
 def _render_single_exercise(exercise: Exercise, pid: str, visit_id: str, ex_key: str, viz=None) -> None:
 
-    if viz is None:
-        viz = Visualizer()
+    try:
+        # load the DataFrame
+        df: pd.DataFrame = exercise.load_dataframe('clean')
+    except FileNotFoundError:
+        return
 
     try:
         # get data for dashboard
-        dashboard_data = _prepare_dashboard_data(exercise)
+        plot_metrics = _extract_plot_metrics(exercise)
 
         # if no metrics were found (e.g., extraction failed) -> skip plotting
-        if not dashboard_data.get('metrics'):
+        if not plot_metrics:
             print(f"Skipping {ex_key} for {pid}...: No metrics found")
             return
 
         # render the dashboard
-        viz.viz_render_dashboard(dashboard_data, pid, visit_id, ex_key, exercise.side_focus, skip_frames=2)
+        viz.viz_render_dashboard(df, plot_metrics, pid, visit_id, ex_key, exercise.side_focus, skip_frames=2)
         print(f"Successfully queued data for {ex_key}")
 
     except Exception as e:
@@ -121,8 +166,12 @@ def check_kinematics():
     participant_pickle_lst: list = [x for x in sorted(os.listdir(participant_objs_path))
                                     if x.startswith('P') and x.endswith('.pickle')]
 
+    if not participant_pickle_lst:
+        print(f"No participant pickle files found in {participant_objs_path}")
+        return
+
     # run for each participant and each visit
-    for pickle_file in participant_pickle_lst:
+    for pickle_file in tqdm(participant_pickle_lst, desc='Rendering Dashboard ...'):
         p = Participant.load(os.path.join(participant_objs_path, pickle_file))
 
         # loop through every exercise this participant performed

src/kinematic_features.py
@@ -199,16 +199,14 @@ class KinematicFeatures:
         if custom_cfg:
             cfg.update(custom_cfg)
 
-        features = {
-            'repetition_freq': 0.0, 'repetition_num': 0.0,
-            'period_mean': 0.0, 'period_pct_90': 0.0, 'period_cov': 0.0,
-            'amplitude_mean': 0.0, 'amplitude_pct_90': 0.0, 'amplitude_cov': 0.0,
-            'velocity_pos_mean': 0.0, 'velocity_pos_pct_90': 0.0, 'velocity_pos_cov': 0.0,
-            'velocity_neg_mean': 0.0, 'velocity_neg_pct_90': 0.0, 'velocity_neg_cov': 0.0,
-            'extraction_status': 'failed',
-            'valid_peaks_idx': [],
-            'valid_valleys_idx': []
-        }
+        features = {'repetition_freq': 0.0, 'repetition_num': 0.0,
+                    'period_mean': 0.0, 'period_pct_90': 0.0, 'period_cov': 0.0,
+                    'amplitude_mean': 0.0, 'amplitude_pct_90': 0.0, 'amplitude_cov': 0.0,
+                    'velocity_pos_mean': 0.0, 'velocity_pos_pct_90': 0.0, 'velocity_pos_cov': 0.0,
+                    'velocity_neg_mean': 0.0, 'velocity_neg_pct_90': 0.0, 'velocity_neg_cov': 0.0,
+                    'extraction_status': 'failed',
+                    'valid_peaks_idx': [],
+                    'valid_valleys_idx': []}
 
         raw_signal = np.squeeze(raw_signal)
 
@@ -250,11 +248,17 @@ class KinematicFeatures:
             det_signal: np.ndarray = sosfiltfilt(sos, signal_data)
             return det_signal
 
+        def _robust_lowpass(signal_data: np.ndarray, high_cutoff: float = 15) -> np.ndarray:
+            if len(signal_data) < 15:
+                return signal_data
+            sos = butter(4, high_cutoff, btype='lowpass', fs=self.fps, output='sos')
+            return sosfiltfilt(sos, signal_data)
+
         # detrend the raw signal
-        detrended_signal: np.ndarray = _robust_detrend(raw_signal, low_cutoff=0.3, high_cutoff=15.0)
+        #detrended_signal: np.ndarray = _robust_detrend(raw_signal, low_cutoff=0.3, high_cutoff=15.0)
+        clean_signal: np.ndarray = _robust_lowpass(raw_signal, high_cutoff=15.0)
 
         # 2) scouting for peaks and valleys using zero-crossings
-
         window_size: int = int(self.fps * 1.0)
 
         # ensure odd kernel size
@@ -262,11 +266,13 @@ class KinematicFeatures:
             window_size += 1
 
         # subtract the rolling median (only for scouting)
-        baseline_trend: np.ndarray = medfilt(volume=detrended_signal, kernel_size=window_size)
-        scout_signal: np.ndarray = detrended_signal - baseline_trend
+        baseline_trend: np.ndarray = medfilt(volume=clean_signal, kernel_size=window_size)
+
+        # create zero-centered signal for peak-finding
+        detrended_signal: np.ndarray = clean_signal - baseline_trend
 
         # extract zero-crossings by from dynamically centered scout signal
-        signs: np.ndarray = np.sign(scout_signal)
+        signs: np.ndarray = np.sign(detrended_signal)
         signs[signs == 0] = 1
         zero_crossings: np.ndarray = np.where(np.diff(signs))[0]
 
@@ -426,6 +432,8 @@ class KinematicFeatures:
 
         # extraction successful
         features['extraction_status'] = 'success'
+        features['signal_original'] = clean_signal
+        features['signal_baseline'] = baseline_trend
         features['signal_detrended'] = detrended_signal
         features['time_axis'] = np.arange(len(detrended_signal)) / self.fps
 

src/loader.py
@@ -2,6 +2,7 @@
 
 # libraries
 import os
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
@@ -134,22 +135,69 @@ def load_participants(parquet_file_paths: list) -> None:
     print('Calculating anatomical reference hand sizes ...')
     for p in tqdm(all_participants.values(), desc='Calculating Participant Hand Size.'):
 
-        # 1) calculate the global hand sizes (left and right) for the current Participant
-        best_hand_ref_dict = tb.determine_best_hand_reference(p)    # Returns: {p.pid: {'Affected': X, 'Healthy': Y}}
-        participant_refs = best_hand_ref_dict.get(p.pid, {})
+        left_sizes: dict = {}
+        right_sizes: dict = {}
 
-        # 2) map the 'Affected' and 'Healthy' keys back to Left/Right
-        if p.affected_side == 'L':
-            left_size = participant_refs.get('Affected', 0.0)
-            right_size = participant_refs.get('Healthy', 0.0)
-        else:
-            left_size = participant_refs.get('Healthy', 0.0)
-            right_size = participant_refs.get('Affected', 0.0)
-
-        # 3) add the calculated sizes to every exercise of the current Participant
         for ex_key, ex in p.exercises.items():
+            left_size, right_size = tb.calc_anatomical_hand_sizes(ex)
+
+            # store hand sizes in corresponding Exercise member variable
             ex.left_hand_size = left_size
             ex.right_hand_size = right_size
 
+            if left_size > 0:
+                left_sizes[ex_key] = left_size
+            if right_size > 0:
+                right_sizes[ex_key] = right_size
+
+        # plausibility check
+        STD_THRESH: float = 0.02
+
+        if len(left_sizes) > 1:
+            l_std = np.std(list(left_sizes.values()))
+            if l_std > STD_THRESH:
+                print(f'\n[!] Warning: High left hand size variance (STD: {l_std:.4f} for {p.pid}.')
+                for key, size in left_sizes.items():
+                    print(f'\t - {key}: {size:.4f}')
+
+        if len(right_sizes) > 1:
+            r_std = np.std(list(right_sizes.values()))
+            if r_std > STD_THRESH:
+                print(f'\n[!] Warning: High right hand size variance (STD: {r_std:.4f} for {p.pid}.')
+                for key, size in right_sizes.items():
+                    print(f'\t - {key}: {size:.4f}')
+
         # 4) Save the current participant object
         p.save(os.path.join(project_path,'data', '03_processed'))
+
+    # generate participant master file
+    print('Generating Participant overview CSV ...')
+    csv_rows: list = []
+    for p in all_participants.values():
+        session_key: str = f'{p.pid}_{p.visit_id}'
+
+        for ex_key, ex in p.exercises.items():
+            try:
+                filename: str = os.path.basename(ex.raw_landmark_data_path)
+                trial_code = filename.split('_')[4]
+            except IndexError:
+                trial_code = 'Unknown'
+
+            csv_rows.append({'session_key': session_key,
+                             'trial_code': trial_code,
+                             'visit_id': ex.visit_id,
+                             'exercise_id': ex.exercise_id,
+                             'side_condition': ex.side_condition,
+                             'side_focus': ex.side_focus,
+                             'left_hand_size': round(ex.left_hand_size, 4),
+                             'right_hand_size': round(ex.right_hand_size, 4)})
+
+    if csv_rows:
+        df_overview: pd.DataFrame = pd.DataFrame(csv_rows)
+
+        # sort by participant -> trial code
+        df_overview.sort_values(by=['session_key', 'trial_code'], inplace=True)
+
+        out_file: str = os.path.join(project_path, 'data', '05_results', '01_ul_tracking', 'participant_overview.csv')
+        df_overview.to_csv(out_file, index=False)
+        print(f'Successfully saved overview for {len(df_overview)} trials.')

src/utils.py
@@ -256,7 +256,7 @@ class ToolBox:
                 arr = hampel(arr, window_size=HAMPEL_WINDOW, n_sigma=HAMPEL_SIGMA).filtered_data
             except Exception:
                 pass
-            valid_df[col] = arr
+            valid_df.loc[:,col] = arr
 
         # Butterworth filtering
         b, a = butter(N=BUTTER_ORDER, Wn=CUTOFF_FREQ, btype='low', fs=self.fps)
@@ -461,140 +461,187 @@ class ToolBox:
 
         return filtered_landmarks
 
-    def determine_best_hand_reference(self, p: Participant) -> dict:
-        """
-        Calculates the median hand sizes of the left and the right hand sides across all exercises of the given participant
-        for a specific visit (e.g., T1 or T2).
+    # def determine_best_hand_reference(self, p: Participant) -> dict:
+    #     """
+    #     Calculates the median hand sizes of the left and the right hand sides across all exercises of the given participant
+    #     for a specific visit (e.g., T1 or T2).
+    #
+    #     Args:
+    #         p (Participant): participant object of the class Participant.
+    #
+    #     Returns:
+    #         best_hand_ref_dict (dict): dictionary holding the largest median hand size for the affected and healthy side
+    #     """
+    #
+    #     # load lists from config file
+    #     link_lst: list[list[str]] = config['body_parts']['hands_link_lst']
+    #     l_hand_size_link_lst: list[str] = config['body_parts']['hand_size_left']
+    #     r_hand_size_link_lst: list[str] = config['body_parts']['hand_size_right']
+    #
+    #     def get_med_hand_size(df: pd.DataFrame, hand_size_link_lst: list[str], occlusion_threshold: float = 0.85,
+    #                           framerate: float = self.fps) -> float:
+    #         """
+    #         Calculates the median hand size using the anatomical hand size (sum of wrist to middle finger knuckle
+    #         plus each middle finger segment) and the direct distance (wrist to middle fingertip) as a threshold measure.
+    #         This function is intended for single hand processing.
+    #
+    #         Args:
+    #             exercise (Exercise): Exercise object with tracked landmarks and exercise information.
+    #             target_side (str): Whether the active or passive hand is targeted ('L' or 'R').
+    #             hand_specific_link_lst (list): List of connected landmark pairs, e.g., [['wrist1', 'cmc11'], ...].
+    #             hand_size_link_lst (list): List of segments from wrist to middle fingertip, e.g., ['wrist1-mcp13', ...].
+    #             occlusion_threshold: Minimum acceptable ratio of straight-line hand size to segmented hand size.
+    #                                  (e.g., 0.7 means straight line must be at least 70% of the segmented length).
+    #             framerate (float): Number of frames per second of the underlying data.
+    #
+    #         Returns:
+    #             float: The median size of the given hand.
+    #         """
+    #
+    #         # length of each hand segment
+    #         num_frames: int = len(df)
+    #         segment_sums: np.ndarray = np.zeros(num_frames)
+    #         valid_mask: np.ndarray = np.ones(num_frames, dtype=bool)
+    #
+    #         try:
+    #             # calculate hand size by the sum of anatomical segments (e.g., wrist1 -> mcp -> pip -> dip -> ftip)
+    #             for link in hand_size_link_lst:
+    #                 j1, j2 = link.split('-')
+    #                 lm1 = df[[f'{j1}_x', f'{j1}_y', f'{j1}_z']].to_numpy()
+    #                 lm2 = df[[f'{j2}_x', f'{j2}_y', f'{j2}_z']].to_numpy()
+    #
+    #                 dist = np.linalg.norm(lm1 - lm2, axis=1)
+    #                 segment_sums += dist
+    #                 valid_mask &= ~np.isnan(dist)   # mark frames with missing joints as invalid
+    #
+    #             # calculate the direct distance (wrist -> ftip)
+    #             start_joint = hand_size_link_lst[0].split('-')[0]
+    #             end_joint = hand_size_link_lst[-1].split('-')[1]
+    #
+    #             pos_start = df[[f'{start_joint}_x', f'{start_joint}_y', f'{start_joint}_z']].to_numpy()
+    #             pos_end = df[[f'{end_joint}_x', f'{end_joint}_y', f'{end_joint}_z']].to_numpy()
+    #             direct_dist = np.linalg.norm(pos_start - pos_end, axis=1)
+    #
+    #             # occlusion filter: hand size segment lengths of flexed hands, i.e., fists should not be included
+    #             if np.any(segment_sums[valid_mask] < direct_dist[valid_mask]):
+    #                 print('Warning: Abnormal values for hand size calculation. '
+    #                       'Direct wrist-ftip distance was larger than the sum of segments.')
+    #
+    #             # apply threshold mask
+    #             with np.errstate(divide='ignore', invalid='ignore'):
+    #                 ratio = direct_dist / segment_sums
+    #
+    #             # replace NaNs with 0.0
+    #             ratio = np.nan_to_num(ratio, nan=0.0, posinf=0.0, neginf=0.0)
+    #
+    #             flat_mask = (ratio > occlusion_threshold) & valid_mask
+    #
+    #             if np.sum(flat_mask) < int(framerate):
+    #                 return 0.0
+    #
+    #             return float(np.median(segment_sums[flat_mask]))
+    #
+    #         except Exception as e:
+    #             print(f'Error: missing columns for hand size calculation: {e}')
+    #
+    #     # variables for results
+    #     best_hand_ref_dict: dict = dict()
+    #     med_results = {'L': [], 'R': []}
+    #     thresh_lst = config['preprocessing']['thresh_lst']  # relax the threshold for cases with severe spasticity
+    #
+    #     # run for each exercise
+    #     for ex_key, exercise in p.exercises.items():
+    #
+    #         # extract side of focus
+    #         hand_of_focus: str = exercise.side_focus
+    #         passive_hand: str = 'R' if hand_of_focus == 'L' else 'L'
+    #
+    #         active_links = l_hand_size_link_lst if hand_of_focus == 'L' else r_hand_size_link_lst
+    #         passive_links = r_hand_size_link_lst if hand_of_focus == 'L' else l_hand_size_link_lst
+    #
+    #         # load the clean parquet file
+    #         try:
+    #             df_clean: pd.DataFrame = exercise.load_dataframe(stage='clean')
+    #         except FileNotFoundError:
+    #             print(f'Skipping {ex_key}: No clean dataframe found for this exercise.')
+    #             continue
+    #
+    #         active_med: float = 0.0
+    #         passive_med: float = 0.0
+    #
+    #         # calculate median hand sizes for different hand aperture threshold until return variable is > 0.0
+    #         # active side
+    #         for thresh in thresh_lst:
+    #             if active_med == 0.0:
+    #                 active_med = get_med_hand_size(df_clean, active_links, thresh, self.fps)
+    #             if passive_med == 0.0:
+    #                 passive_med = get_med_hand_size(df_clean, passive_links, thresh, self.fps)
+    #
+    #             if active_med > 0.0 and passive_med > 0.0:
+    #                 break
+    #
+    #         if active_med == 0.0 or passive_med == 0.0:
+    #             print(f'Warning: "{ex_key}" ({p.pid}, {p.visit_id}) yielded no median for one or both hand size.')
+    #
+    #         med_results[hand_of_focus].append(active_med)
+    #         med_results[passive_hand].append(passive_med)
+    #
+    #     # select the largest median hand size
+    #     max_left: float = max(med_results['L']) if med_results['L'] else 0.0
+    #     max_right: float = max(med_results['R']) if med_results['R'] else 0.0
+    #
+    #     # add the selected hand size to the dictionary
+    #     if p.affected_side == 'L':
+    #         best_hand_ref_dict[p.pid] = {'Affected': max_left, 'Healthy': max_right}
+    #
+    #     elif p.affected_side == 'R':
+    #         best_hand_ref_dict[p.pid] = {'Affected': max_right, 'Healthy': max_left}
+    #
+    #     return best_hand_ref_dict
 
-        Args:
-            p (Participant): participant object of the class Participant.
-
-        Returns:
-            best_hand_ref_dict (dict): dictionary holding the largest median hand size for the affected and healthy side
-        """
+    @staticmethod
+    def calc_anatomical_hand_sizes(exercise: Exercise) -> tuple[float, float]:
 
         # load lists from config file
-        link_lst: list[list[str]] = config['body_parts']['hands_link_lst']
-        l_hand_size_link_lst: list[str] = config['body_parts']['hand_size_left']
-        r_hand_size_link_lst: list[str] = config['body_parts']['hand_size_right']
+        l_links: list[str] = config['body_parts']['hand_size_left']
+        r_links: list[str] = config['body_parts']['hand_size_right']
 
-        def get_med_hand_size(df: pd.DataFrame, hand_size_link_lst: list[str], occlusion_threshold: float = 0.85,
-                              framerate: float = self.fps) -> float:
-            """
-            Calculates the median hand size using the anatomical hand size (sum of wrist to middle finger knuckle
-            plus each middle finger segment) and the direct distance (wrist to middle fingertip) as a threshold measure.
-            This function is intended for single hand processing.
+        try:
+            df: pd.DataFrame = exercise.load_dataframe('clean')
+        except FileNotFoundError:
+            return 0.0, 0.0
 
-            Args:
-                exercise (Exercise): Exercise object with tracked landmarks and exercise information.
-                target_side (str): Whether the active or passive hand is targeted ('L' or 'R').
-                hand_specific_link_lst (list): List of connected landmark pairs, e.g., [['wrist1', 'cmc11'], ...].
-                hand_size_link_lst (list): List of segments from wrist to middle fingertip, e.g., ['wrist1-mcp13', ...].
-                occlusion_threshold: Minimum acceptable ratio of straight-line hand size to segmented hand size.
-                                     (e.g., 0.7 means straight line must be at least 70% of the segmented length).
-                framerate (float): Number of frames per second of the underlying data.
+        def _get_size(link_lst: list[str]) -> float:
+            # safety check for missing wrist
+            start_joint = link_lst[0].split('-')[0]
+            if f"{start_joint}_x" not in df.columns:
+                return 0.0
 
-            Returns:
-                float: The median size of the given hand.
-            """
-
-            # length of each hand segment
             num_frames: int = len(df)
             segment_sums: np.ndarray = np.zeros(num_frames)
-            valid_mask: np.ndarray = np.ones(num_frames, dtype=bool)
+            valid_mask = np.ones(num_frames, dtype=bool)
 
-            try:
-                # calculate hand size by the sum of anatomical segments (e.g., wrist1 -> mcp -> pip -> dip -> ftip)
-                for link in hand_size_link_lst:
-                    j1, j2 = link.split('-')
-                    lm1 = df[[f'{j1}_x', f'{j1}_y', f'{j1}_z']].to_numpy()
-                    lm2 = df[[f'{j2}_x', f'{j2}_y', f'{j2}_z']].to_numpy()
-
-                    dist = np.linalg.norm(lm1 - lm2, axis=1)
+            for link in link_lst:
+                j1, j2 = link.split('-')
+                try:
+                    pos1 = df[[f'{j1}_x', f'{j1}_y', f'{j1}_z']].to_numpy()
+                    pos2 = df[[f'{j2}_x', f'{j2}_y', f'{j2}_z']].to_numpy()
+                    dist = np.linalg.norm(pos1 - pos2, axis=1)
                     segment_sums += dist
-                    valid_mask &= ~np.isnan(dist)   # mark frames with missing joints as invalid
-
-                # calculate the direct distance (wrist -> ftip)
-                start_joint = hand_size_link_lst[0].split('-')[0]
-                end_joint = hand_size_link_lst[-1].split('-')[1]
-
-                pos_start = df[[f'{start_joint}_x', f'{start_joint}_y', f'{start_joint}_z']].to_numpy()
-                pos_end = df[[f'{end_joint}_x', f'{end_joint}_y', f'{end_joint}_z']].to_numpy()
-                direct_dist = np.linalg.norm(pos_start - pos_end, axis=1)
-
-                # occlusion filter: hand size segment lengths of flexed hands, i.e., fists should not be included
-                if np.any(segment_sums[valid_mask] < direct_dist[valid_mask]):
-                    print('Warning: Abnormal values for hand size calculation. '
-                          'Direct wrist-ftip distance was larger than the sum of segments.')
-
-                # apply threshold mask
-                with np.errstate(divide='ignore', invalid='ignore'):
-                    ratio = direct_dist / segment_sums
-
-                flat_mask = (ratio > occlusion_threshold) & valid_mask
-
-                if np.sum(flat_mask) < int(framerate):
+                    valid_mask &= ~np.isnan(dist)
+                except KeyError:
+                    # missing landmarks
                     return 0.0
 
-                return float(np.median(segment_sums[flat_mask]))
+            if not np.any(valid_mask):
+                return 0.0
 
-            except Exception as e:
-                print(f'Error: missing columns for hand size calculation: {e}')
+            return float(np.median(segment_sums[valid_mask]))
 
-        # variables for results
-        best_hand_ref_dict: dict = dict()
-        med_results = {'L': [], 'R': []}
-        thresh_lst = config['preprocessing']['thresh_lst']  # relax the threshold for cases with severe spasticity
+        left_size: float = _get_size(l_links)
+        right_size: float = _get_size(r_links)
 
-        # run for each exercise
-        for ex_key, exercise in p.exercises.items():
-
-            # extract side of focus
-            hand_of_focus: str = exercise.side_focus
-            passive_hand: str = 'R' if hand_of_focus == 'L' else 'L'
-
-            active_links = l_hand_size_link_lst if hand_of_focus == 'L' else r_hand_size_link_lst
-            passive_links = r_hand_size_link_lst if hand_of_focus == 'L' else l_hand_size_link_lst
-
-            # load the clean parquet file
-            try:
-                df_clean: pd.DataFrame = exercise.load_dataframe(stage='clean')
-            except FileNotFoundError:
-                print(f'Skipping {ex_key}: No clean dataframe found for this exercise.')
-                continue
-
-            active_med: float = 0.0
-            passive_med: float = 0.0
-
-            # calculate median hand sizes for different hand aperture threshold until return variable is > 0.0
-            # active side
-            for thresh in thresh_lst:
-                if active_med == 0.0:
-                    active_med = get_med_hand_size(df_clean, active_links, thresh, self.fps)
-                if passive_med == 0.0:
-                    passive_med = get_med_hand_size(df_clean, passive_links, thresh, self.fps)
-
-                if active_med > 0.0 and passive_med > 0.0:
-                    break
-
-            if active_med == 0.0 or passive_med == 0.0:
-                print(f'Warning: "{ex_key}" ({p.pid}, {p.visit_id}) yielded no median for one or both hand size.')
-
-            med_results[hand_of_focus].append(active_med)
-            med_results[passive_hand].append(passive_med)
-
-        # select the largest median hand size
-        max_left: float = max(med_results['L']) if med_results['L'] else 0.0
-        max_right: float = max(med_results['R']) if med_results['R'] else 0.0
-
-        # add the selected hand size to the dictionary
-        if p.affected_side == 'L':
-            best_hand_ref_dict[p.pid] = {'Affected': max_left, 'Healthy': max_right}
-
-        elif p.affected_side == 'R':
-            best_hand_ref_dict[p.pid] = {'Affected': max_right, 'Healthy': max_left}
-
-        return best_hand_ref_dict
+        return left_size, right_size
 
     @staticmethod
     def calculate_3d_segment_lengths(df: pd.DataFrame, link_lst: list[list[str]]) -> pd.DataFrame:
@@ -911,6 +958,7 @@ class ToolBox:
             aligned_dict[k] = P_rot_T.T
 
         return aligned_dict
+
 
 def infer_focus_side(df: pd.DataFrame, model_type: str = 'Hand') -> str | None:
     """

src/visualization.py
@@ -477,8 +477,12 @@ class Visualizer:
         # plot the main signal
         ax.plot(time_axis, signal, color='black', linewidth=1.5, alpha=0.7, label=ex_id, zorder=2)
 
-        # zero-crossing baseline
-        ax.axhline(0, color='gray', linestyle='--', label='zero-crossing baseline', zorder=1)
+        # dynamic baseline
+        baseline = features.get('signal_baseline', [])
+        if len(baseline) > 0:
+            ax.plot(time_axis, baseline, color='grey', linestyle='--', label='dynamic baseline', zorder=1)
+        else:
+            ax.axhline(0, color='gray', linestyle='--', label='zero-crossing baseline', zorder=1)
 
         # plot valid peaks (green up-triangles)
         peak_indices = features.get('valid_peaks_idx', [])
@@ -531,13 +535,14 @@ class Visualizer:
     # ============================================================================= #
     #                            4) KINEMATICS QUALITY CHECK                        #
     # ============================================================================= #
-    def viz_render_dashboard(self, dashboard_data: dict, p_id: str, visit_id: str, ex_id: str,
+    def viz_render_dashboard(self, df:pd.DataFrame, metrics_dict: dict, p_id: str, visit_id: str, ex_id: str,
                              side_focus: str, skip_frames: int = 2) -> None:
         """
         Renders a modular quality control dashboard and saves it as a video file (mp4).
 
         Args:
-            dashboard_data (dict): Dictionary containing pose, hands, and metrics.
+            df (pd.DataFrame): dataframe with movement data.
+            metrics_dict (dict): Dictionary containing pose, hands, and metrics.
             p_id (str): Participant identifier key.
             visit_id (str): Visit ID.
             ex_id (str): Experiment identifier key.
@@ -548,7 +553,48 @@ class Visualizer:
             None
         """
 
-        metrics_dict = dashboard_data.get('metrics', {})
+        # nested dictionaries for 3D plotter
+        dashboard_data = {'left_hand': {}, 'right_hand': {}, 'pose': {}}
+
+        # helper to extract a (3, frames) list of arrays for a specific joint
+        def get_joint_data(joint_name):
+            if f"{joint_name}_x" in df.columns:
+                return [df[f"{joint_name}_x"].tolist(),
+                        df[f"{joint_name}_y"].tolist(),
+                        df[f"{joint_name}_z"].tolist()]
+            return None
+
+        # Safe identifier function
+        def get_side_id(joint_name):
+            for char in joint_name:
+                if char.isdigit():
+                    return char
+            return None
+
+        # left hand dict
+        for link in config['body_parts'].get('hands_link_lst', []):
+            for joint in link:
+                if get_side_id(joint) == '1' and joint not in dashboard_data['left_hand']:
+                    data = get_joint_data(joint)
+                    if data:
+                        dashboard_data['left_hand'][joint] = data
+
+        # right hand dict
+        for link in config['body_parts'].get('hands_link_lst', []):
+            for joint in link:
+                if get_side_id(joint) == '2' and joint not in dashboard_data['right_hand']:
+                    data = get_joint_data(joint)
+                    if data:
+                        dashboard_data['right_hand'][joint] = data
+
+        # pose dict
+        for link in config['body_parts'].get('pose_link_lst', []):
+            for joint in link:
+                if joint not in dashboard_data['pose']:
+                    data = get_joint_data(joint)
+                    if data:
+                        dashboard_data['pose'][joint] = data
+
         num_metrics = len(metrics_dict)
 
         # ensure skip_frames is at least 1 (skip_frames == 1: use every frame, skip_frames == 2: use every 2nd frame)
@@ -596,8 +642,9 @@ class Visualizer:
             ax.set_zlim([np.min(all_z) - pad, np.max(all_z) + pad])
 
             # invert axes to transform between MediaPipe and matplotlib coordinate systems
-            ax.invert_yaxis()       # MediaPipe's y-axis is positive-down
-            ax.invert_xaxis()       # x-axis mirrors the skeleton on the Y-Z plane
+            ax.invert_yaxis()
+            ax.invert_xaxis()
+            ax.invert_zaxis()
 
             ax.set_xticklabels([])
             ax.set_yticklabels([])