04ccc64b101766d18c66cd99bf13a91abb0b101a
kmoeri <kevin.moeri@outlook.com>
2026-05-04 17:21:51 +0200

    Minor changes

--- src/data_split.py
+++ src/data_split.py
@@ -3,24 +3,20 @@
 # libraries
 import pandas as pd
 
-# modules
-from src.config import config, project_path
 
-
-def load_and_split_data(features_path: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+def load_and_split_data(features_path: str, test_pids: list[str], test_visit: str) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Loads extracted features and splits into train and test sets.
     For test participants only the baseline visit (T1) is used. Subsequent visits (T2 & T3) are discarded.
 
     Args:
         features_path (str): Path to extracted features file.
+        test_pids (list[str]): List of test participants.
+        test_visit (str): Test visit ID (baseline only).
 
     Returns:
         tuple[pd.DataFrame, pd.DataFrame]: Two DataFrames separating the train and test sets.
     """
-
-    test_pids: list[str] = config['classification']['test_participants']
-    test_visit: str = config['classification']['baseline_visit_id']
 
     df: pd.DataFrame = pd.read_csv(features_path)
 
